b55a21f98ab21c004b80a9e181ecd0ed89e82123
PITTAWAT <pitts457@cubes.postbox.in.th>
2023-07-16 01:01:37 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,49 @@
 <template>
-  <router-view />
+  <v-app id="home">
+    <NavBar />
+
+    <!-- =============  search  ============= -->
+    <Search />
+    <!-- =============  search  ============= -->
+
+    <!-- =============  book detail  ============= -->
+    <Book :val="BookData" />
+    <!-- =============  book detail  ============= -->
+
+    <Footer />
+    <Scroll />
+  </v-app>
 </template>
 
-<script setup>
-  //
+<script>
+import { defineComponent, ref } from "vue";
+import NavBar from "@/components/NavBar.vue";
+import Book from "@/components/Book.vue";
+import Footer from "@/components/Footer.vue";
+import Scroll from "@/components/Scroll.vue";
+import Search from "@/components/Search.vue";
+
+export default defineComponent({
+  name: "HomeView",
+  components: {
+    NavBar,
+    Book,
+    Footer,
+    Scroll,
+    Search,
+  },
+  data: () => ({
+    default: {
+      text: "react",
+    },
+  }),
+  computed: {
+    BookData() {
+      return this.$store.state.BookData;
+    },
+  },
+  mounted() {
+    this.$store.dispatch("fetchData", this.default);
+  },
+});
 </script>
